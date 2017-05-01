fc317efa2eb6793e2ba97bac83ea8146a0dc3f8b
wuyihao <ambulance@mail.dlut.edu.cn>
2017-04-30 19:12:27 -0800

    workflow prepare work amend

--- workflow.sh
+++ workflow.sh
@@ -1,17 +1,21 @@
+if ! [ -d "/tmp/ns" ]; then
+	cd /tmp/
+	yaourt -G ns
+fi
 sh -c "cd ~/wsn/ns-2.35/diffusion3/apps/ping; echo Start Coding; zsh"
 echo "Start Building"
 
 if [ "$1" != "try" ]; then
 	cd ~/wsn
 	tar czf ns-2.35.tar.gz ns-2.35
-	cd /tmp/yaourt-tmp-wuyihao/aur-ns
+	cd /tmp/ns
 
 	mv ~/wsn/ns-2.35.tar.gz ./ns-src-2.35.tar.gz
 	if makepkg -f --skipchecksums 2>&1 > /tmp/buildlog ; then
 		makepkg -i
 	fi
 else
-	cd /tmp/yaourt-tmp-wuyihao/aur-ns
+	cd /tmp/ns
 	echo "'./workflow final' guarantees the build result, but very slow"
 	rsync -avr ~/wsn/ns-2.35 ./src/
 	cd ./src/ns-2.35
@@ -20,4 +24,3 @@ else
 		makepkg -i
 	fi
 fi
-
