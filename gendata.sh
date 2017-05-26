5d8662391861fa1c6ed38c19bfcb0937646d35eb
wuyihao <ambulance@mail.dlut.edu.cn>
2017-05-25 20:54:03 -0800

    Location put in gendata

--- gendata.sh
+++ gendata.sh
@@ -2,6 +2,25 @@
 
 echo > data_flow
 echo > charging_request
+echo > location
+
+cat > location << EOF
+\$node_(0) set X_ 0.0
+\$node_(0) set Y_ 25.0
+\$node_(0) set Z_ 0.000000000000
+\$node_(1) set X_ 10.0
+\$node_(1) set Y_ 10.0
+\$node_(1) set Z_ 0.000000000000
+\$node_(2) set X_ 5.0
+\$node_(2) set Y_ 0.0
+\$node_(2) set Z_ 0.000000000000
+\$node_(3) set X_ 5.0
+\$node_(3) set Y_ 5.0
+\$node_(3) set Z_ 0.000000000000
+\$node_(4) set X_ 0.0
+\$node_(4) set Y_ 0.0
+\$node_(4) set Z_ 0.000000000000
+EOF
 
 
 #N=30; (for i in `seq $N`; do for j in `seq 0 1`; do echo "\$ns_ at "$((i*(20.1/$N)+70)) '"$con_('$j') publish"'; done; echo "\$ns_ at "$(((i+1)*(20.1/$N)+70)) '"$snk_ subscribe"'; done; )|tee -a data_flow
