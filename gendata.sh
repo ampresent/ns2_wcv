d3b665da1ddb681715883184a48418d828182af9
wuyihao <wuyihao13@gmail.com>
2017-07-06 14:19:45 +0800

    Bigger scale

--- gendata.sh
+++ gendata.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
-nn=7
+nn=14
+x=50
+y=50
 stop=2000
 echo > initialize
 echo > data_flow
@@ -9,8 +11,8 @@ echo > location
 
 cat > initialize << EOF
 set val(nn)             $nn                         ;# number of mobilenodes
-set val(x)		50
-set val(y)		50
+set val(x)		$x
+set val(y)		$y
 set wcv			$((nn-1))
 set base		$((nn-2))
 set malicious {`shuf -i 0-$((nn-3)) -n $(((nn-2) / 1)) | xargs `}            
@@ -18,30 +20,18 @@ set val(stop)		$stop
 EOF
 # | tr "\n" "," | sed "s/,$//g"`}
 
-cat > location << EOF
-\$node_(0) set X_ 20.0
-\$node_(0) set Y_ -10.0
-\$node_(0) set Z_ 0.000000000000
-\$node_(1) set X_ 20.0
-\$node_(1) set Y_ 10.0
-\$node_(1) set Z_ 0.000000000000
-\$node_(2) set X_ 10.0
-\$node_(2) set Y_ 0.0
-\$node_(2) set Z_ 0.000000000000
-\$node_(5) set X_ 30.0
-\$node_(5) set Y_ 0.0
-\$node_(5) set Z_ 0.000000000000
-\$node_(6) set X_ 0.0
-\$node_(6) set Y_ 0.0
-\$node_(6) set Z_ 0.000000000000
-\$node_(3) set X_ 0
-\$node_(3) set Y_ 10
-\$node_(3) set Z_ 0
-\$node_(4) set X_ 0
-\$node_(4) set Y_ -10
-\$node_(4) set Z_ 0
+cat > /tmp/location.py << EOF
+import random
+X = $x
+Y = $y
+for i in range($nn):
+  x = random.uniform(0, $x)
+  y = random.uniform(0, $y)
+  print "\$node_({}) set X_ {}".format(i ,x)
+  print "\$node_({}) set Y_ {}".format(i, y)
+  print "\$node_({}) set Z_ 0.000000000000".format(i)
 EOF
-
+python /tmp/location.py > ./location
 
 #N=30; (for i in `seq $N`; do for j in `seq 0 1`; do echo "\$ns_ at "$((i*(20.1/$N)+70)) '"$con_('$j') publish"'; done; echo "\$ns_ at "$(((i+1)*(20.1/$N)+70)) '"$snk_ subscribe"'; done; )|tee -a data_flow
 
