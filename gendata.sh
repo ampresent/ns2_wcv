fab4bbe352d0cd648a2409f91f47725093dade0e
wuyihao <wuyihao13@gmail.com>
2017-07-06 15:58:21 +0800

    typo fix

--- gendata.sh
+++ gendata.sh
@@ -31,7 +31,9 @@ for i in range($nn):
   print "\$node_({}) set Y_ {}".format(i, y)
   print "\$node_({}) set Z_ 0.000000000000".format(i)
 EOF
-python /tmp/location.py > ./location
+python2 /tmp/location.py > ./location
+
+cat ./location
 
 #N=30; (for i in `seq $N`; do for j in `seq 0 1`; do echo "\$ns_ at "$((i*(20.1/$N)+70)) '"$con_('$j') publish"'; done; echo "\$ns_ at "$(((i+1)*(20.1/$N)+70)) '"$snk_ subscribe"'; done; )|tee -a data_flow
 
