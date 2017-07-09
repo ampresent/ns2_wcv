f0b2f397ccdddfa94f5cb7342a6e982633d0a05c
wuyihao <wuyihao13@gmail.com>
2017-07-09 20:35:35 +0800

    energy curve

--- energy_curve.sh
+++ energy_curve.sh
@@ -10,14 +10,15 @@ EOF
 
 
 for i in $@; do 
+	ag '\-n '$i' \-e' scenario1.tr >/dev/null 2>&1 || continue
+
 	points="/tmp/points_$i"
 	echo > $points
-	ag '\-n '$i' \-e' scenario1.tr | awk '{print $3,$7}' >> $points
-	echo 'e' >> $points
+	ag '\-n '$i' \-e' scenario1.tr | awk '{print $3,$7}' >> $points && echo 'e' >> $points
 	request=`mktemp`
 	grep 'Node'$i': Sending WCV Data' log | awk '{print $1}' > $request
 	request_full=`mktemp`
-	awk 'NR==FNR{req[$1]=$2} NR>FNR{print $1,",",req[$1]}' $points $request > $request_full
+	awk 'NR==FNR{req[$1]=$2} NR>FNR{if($1 in req){print $1,",",req[$1]}}' $points $request > $request_full
 
 	cat $request_full | while read line; do
 		echo "set object circle at $line size 0.5" >> $script
@@ -26,8 +27,9 @@ for i in $@; do
 	#echo 'plot - title "Node: $1" with lines' >> $script
 done
 echo -n 'plot ' >> $script
-for i in $@; do 
-	echo -n "'/tmp/points_$i' title 'Node: $i' with lines," >> $script
+for p in /tmp/points_*; do
+	i=$(echo $p|sed 's/[^0-9]//g')
+	echo -n "'$p' title 'Node: $i' with lines," >> $script
 done
 
 LD_LIBRARY_PATH=.  cat $points | gnuplot $script -p
