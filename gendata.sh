6ab0767b5984770fc537c802fae79aa4df76e6c2
wuyihao <wuyihao13@gmail.com>
2017-07-11 16:32:26 +0800

    More data flow, and sink subscribe periodically

--- gendata.sh
+++ gendata.sh
@@ -39,6 +39,8 @@ cat ./location
 
 #N=30; (for i in `seq $N`; do for j in `seq 0 1`; do echo "\$ns_ at "$((i*(20.1/$N)+70)) '"$con_('$j') publish"'; done; echo "\$ns_ at "$(((i+1)*(20.1/$N)+70)) '"$snk_ subscribe"'; done; )|tee -a data_flow
 
-N=40; (for i in `seq $N`; do for j in `seq 0 $((nn-3))`; do rand_int=$(shuf -i 1-1000000 -n 1); t=$(echo "$rand_int*$cease/1000000" | bc -l); echo "\$ns_ at $t "'"$con_('$j') publish"'; done; rand_int=$(shuf -i 1-1000000 -n 1); t=$(echo "$rand_int*$cease/1000000" | bc -l); echo "\$ns_ at $t "'"$snk_ subscribe"'; done; )|tee data_flow
+N=$((stop/4)); (for i in `seq $N`; do for j in `seq 0 $((nn-3))`; do rand_int=$(shuf -i 1-1000000 -n 1); t=$(echo "$rand_int*$stop/1000000" | bc -l); echo "\$ns_ at $t "'"$con_('$j') publish"'; done;  done; )|tee data_flow
 
-N=20; (for i in `seq $N`; do for j in `seq 0 $((nn-3))`; do rand_int=$(shuf -i 1-1000000 -n 1); t=$(echo "$rand_int*$cease/1000000" | bc -l); auto=""; [ $(bc<<<"$t>$war") -eq 1 ] && auto='auto'; echo "\$ns_ at $t "'"$src_('$j') publish '$auto'"'; done; done;echo "\$ns_ at 0 "'"$wcv_ subscribe"'; )|tee charging_request
+for t in `seq 0 15 $stop`; do
+	echo "\$ns_ at $t "'"$snk_ subscribe"' >> data_flow;
+done
